d19be0a2b0d662b9a15d520b9b5da2f8825191ec
YelyzavetaKareieva-S <YelyzavetaKareieva-S@S-FK1H504-SL21>
2026-05-21 13:04:27 +0100

    add training session class

--- Exam2026/Member.cs
+++ Exam2026/Member.cs
@@ -15,4 +15,13 @@ namespace Exam2026
         public string ContactNumber { get; set; }
         public string MembershipStyle { get; set; }
     }
+
+    public class TrainingSession
+    {
+        public int SessionID { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+    }
 }
