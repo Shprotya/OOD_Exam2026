3399f08a8676858cf23067430a9464853db19c22
YelyzavetaKareieva-S <YelyzavetaKareieva-S@S-FK1H504-SL21>
2026-05-21 13:06:00 +0100

    Add a one to many relationship

--- Exam2026/Member.cs
+++ Exam2026/Member.cs
@@ -8,20 +8,30 @@ namespace Exam2026
 {
     public class Member
     {
+        //Properties
         public int MemberID { get; set; }
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string ContactNumber { get; set; }
         public string MembershipStyle { get; set; }
+
+        //Navigation property
+        //A member can have multiple training sessions, so we use a list to represent this relationship.
+        public List<TrainingSession> TrainingSessions { get; set; }
     }
 
     public class TrainingSession
     {
+        //Properties
         public int SessionID { get; set; }
         public DateTime SessionDate { get; set; }
         public string SessionType { get; set; }
         public int DurationMinutes { get; set; }
         public string CoachNotes { get; set; }
+
+        //Navigation property
+        //Each training session is associated with one member, so we use a reference to represent this relationship.
+        public Member Member { get; set; }
     }
 }
