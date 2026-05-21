27ba80120e15e8d8677d1a6e0b9cd3382494a6aa
YelyzavetaKareieva-S <YelyzavetaKareieva-S@S-FK1H504-SL21>
2026-05-21 13:03:01 +0100

    add member class

---
+++ Exam2026/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2026
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipStyle { get; set; }
+    }
+}
